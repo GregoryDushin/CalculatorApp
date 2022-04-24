28c90557241eb28807aeed5ae4adfa37d60c8f7f
GregoryDushin <86513789+GregoryDushin@users.noreply.github.com>
2022-04-24 16:22:22 +0300

    Calculator
    Calculator

---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,53 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Григорий Душин on 23.04.2022.
+//
+
+import Foundation
+
+struct CalculatorLogic {
+    
+   private var number : Double?
+    
+   private var imtermediateCalculation: (n1: Double, calcMethod: String)?
+    
+   mutating func setNumber(_ number: Double){
+        self.number = number
+    }
+  
+    
+    mutating func calculate (symbol: String) -> Double? {
+        
+        if let n = number {
+            switch symbol {
+            case "+/-":
+                return n * -1
+            case "AC":
+                return 0
+            case "%":
+                return n * 0.01
+            case "=":
+                return performTwoNumberCalculation(n2: n)
+            default:
+                imtermediateCalculation = (n1: n, calcMethod: symbol)
+            }
+        }
+      return nil
+    }
+    private func  performTwoNumberCalculation(n2: Double) -> Double? {
+        if let n1 = imtermediateCalculation?.n1 , let operation = imtermediateCalculation?.calcMethod {
+            switch operation {
+            case "+" : return n1 + n2
+            case "-" : return n1 - n2
+            case "÷" : return n1 / n2
+            case "*" : return n1 * n2
+                
+            default: fatalError("Error")
+            }
+            
+        }
+        return nil
+    }
+}
